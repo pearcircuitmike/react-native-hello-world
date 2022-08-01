ed553989083f2e51d5a624a144d9b1f278bb2bc0
Michael Young <msyoung2012@gmail.com>
2022-08-01 19:40:30 -0400

    Passing props in a reusable component
    Just like normal web react more or less.

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      {/* today's tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's task</Text>
+        <View style={styles.items}>
+          <Task text={"Text 1"} />
+          <Task text={"Text 2"} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#e8eaed",
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {},
 });
